451faedf7a34f20db715e152f888eda280ce96f1
Mozahedul <mozahed001@gmail.com>
2021-10-13 16:50:26 +0600

    Fetching Products From Backend to Frontend Using useEffect & axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Col, Row } from 'react-bootstrap';
-import products from '../products';
+// import products from '../products';
 import Product from '../components/Product';
 
 const HomeScreen = () => {
-  console.log(products);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Row>
+        <h1>Latest Products</h1>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
