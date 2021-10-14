09ac3d05dac257479ade0ba15904c35e54244ee5
Mozahedul <mozahed001@gmail.com>
2021-10-14 23:12:16 +0600

    Seeder Script Setup For Importing Dummy Data Into Database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import colors from 'colors';
 
 const connectDB = async () => {
   // For remote server
-//   const PORT = process.env.MONGO_URI;
+  const PORT = process.env.MONGO_URI;
   // For local server
-  const PORT = process.env.LOCAL_MONGO_URI;
+  // const PORT = process.env.LOCAL_MONGO_URI;
   try {
     const conn = await mongoose.connect(PORT, {
       useNewUrlParser: true,
@@ -13,7 +13,7 @@ const connectDB = async () => {
     });
 
     console.log(
-      colors.cyan.bold.underline(`MongoDB connected: ${conn.connection.host}`),
+      colors.cyan.bold.underline(`MongoDB connected: ${conn.connection.host}`)
     );
   } catch (error) {
     console.log(colors.red.bold.underline(`Error: ${error.message}`));
---
+++ backend/seeder.js
@@ -0,0 +1,50 @@
+import colors from 'colors';
+import dotenv from 'dotenv';
+import users from './data/users.js';
+import products from './data/products.js';
+import Product from './models/productModel.js';
+import User from './models/userModel.js';
+import Order from './models/orderModel.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB();
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUser = await User.insertMany(users);
+    const adminUser = createdUser[0]._id;
+    const sampleProduct = products.map(product => {
+      return { ...product, user: adminUser };
+    });
+    await Product.insertMany(sampleProduct);
+    console.log(colors.green.inverse(`Data Imported From Seeder Script`));
+    process.exit();
+  } catch (error) {
+    console.log(colors.red.inverse`${error}`);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log(colors.red.inverse('Data Destroyed With Seeder Script'));
+    process.exit();
+  } catch (error) {
+    console.log(colors.red.inverse(`${error}`));
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
+}
