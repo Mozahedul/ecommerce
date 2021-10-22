8c9cd0de0c7f6839334ab458c8e8bea4606eefef
Mozahedul <mozahed001@gmail.com>
2021-10-23 00:47:53 +0600

    Update To Paid Endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -67,4 +67,33 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+/**
+ * @async
+ * @function updateOrderToPaid
+ * @desc Update order to paid
+ * Route - GET /api/order/:id/pay
+ * Access - Private
+ * @category Backend
+ */
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
