86d37e37f85194ed3ed3514de891cd256fbe1488
Mozahedul <mozahed001@gmail.com>
2021-10-26 20:03:37 +0600

    Admin Screen Access Security

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -6,15 +6,22 @@ import { listUsers } from '../actions/userActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-const UserListScreen = ({ match, location, history }) => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
 
   const userList = useSelector(state => state.userList);
   const { loading, error, users } = userList;
 
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin;
+
   useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push('/login');
+    }
+  }, [dispatch, history, userInfo]);
 
   const deleteHandler = () => {
     console.log('delete User');
