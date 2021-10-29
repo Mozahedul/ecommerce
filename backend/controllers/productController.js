d31e8aaafefd23fde9412803f9aa71c2448f1932
Mozahedul <mozahed001@gmail.com>
2021-10-29 20:34:58 +0600

    Create & Update Product Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,8 +7,6 @@ import Product from '../models/productModel.js';
  * @desc Fetching all products from database
  * - Route - GET /api/products
  * - access - Public
- * @param {Object} req - request object to get data from server
- * @param {Object} res - response object to send data to server
  * @category Backend
  */
 const getProduct = asyncHandler(async (req, res) => {
@@ -22,8 +20,6 @@ const getProduct = asyncHandler(async (req, res) => {
  * @desc Fetching single product from database
  * - Route - GET /api/products/:id
  * - access - Public
- * @param {Object} req - request object to get data from server
- * @param {Object} res - response object to send data to server
  * @category Backend
  */
 const getProductById = asyncHandler(async (req, res) => {
@@ -48,4 +44,79 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProduct, getProductById, deleteProduct };
+/**
+ * @async
+ * @function createProduct
+ * @desc create a product
+ * - Route POST /api/products
+ * - Access Private/Admin
+ * @category Backend
+ */
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'sample product',
+    price: 0,
+    user: req.user._id,
+    description: 'sample description',
+    countInStock: 0,
+    rating: 4,
+    brand: 'apple',
+    numReviews: 3,
+    image: '/image/sample.jpg',
+    category: 'electronics',
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+/**
+ * @async
+ * @function updateProduct
+ * @desc Update a product
+ * - Route POST /api/products/:id
+ * - Access Private/Admin
+ * @category Backend
+ */
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    countInStock,
+    rating,
+    brand,
+    numReviews,
+    image,
+    category,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name || product.name;
+    product.price = price || product.price;
+    product.description = description || product.description;
+    product.countInStock = countInStock || product.countInStock;
+    product.rating = rating || product.rating;
+    product.brand = brand || product.brand;
+    product.numReviews = numReviews || product.numReviews;
+    product.image = image || product.image;
+    product.category = category || product.category;
+
+    const updatedProduct = await product.save();
+    res.status(201).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error();
+  }
+});
+
+export {
+  getProduct,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import {
+  createProduct,
   deleteProduct,
   getProduct,
   getProductById,
+  updateProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getProduct);
+router.route('/').get(getProduct).post(protect, admin, createProduct);
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
