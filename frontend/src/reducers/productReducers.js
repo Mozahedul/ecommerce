871d7ffecaf9c021bc8366b7a61da6d87a2e922c
Mozahedul <mozahed001@gmail.com>
2021-10-30 20:45:29 +0600

    Admin Update Product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -12,7 +12,21 @@ import {
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_RESET,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_RESET,
+  PRODUCT_UPDATE_FAIL,
 } from '../constants/productConstants';
+
+/**
+ * function productListReducer
+ * @desc a reducer function
+ * - Accepts payload from action creator
+ * @param {Object} state
+ * @param {Object} action
+ * @returns state, payload, error, and success message
+ * @category Frontend
+ */
 
 export const productListReducers = (state = { products: [] }, action) => {
   switch (action.type) {
@@ -29,6 +43,16 @@ export const productListReducers = (state = { products: [] }, action) => {
       return state;
   }
 };
+
+/**
+ * @function productDetailsReducers
+ * @desc a reducer function
+ * - Accepts payload from action creator
+ * @param {Object} state
+ * @param {Object} action
+ * @returns state, payload, and error message
+ * @category Frontend
+ */
 
 export const productDetailsReducers = (
   state = { product: { reviews: [] } },
@@ -49,6 +73,16 @@ export const productDetailsReducers = (
   }
 };
 
+/**
+ * @function productDeleteReducer
+ * @desc a reducer function
+ * Accepts payload from action creator
+ * @param {Object} state
+ * @param {Object} action
+ * @returns state, payload, and error message
+ * @category Frontend
+ */
+
 export const productDeleteReducer = (state = {}, action) => {
   switch (action.type) {
     case PRODUCT_DELETE_REQUEST:
@@ -65,6 +99,16 @@ export const productDeleteReducer = (state = {}, action) => {
   }
 };
 
+/**
+ * @function productCreateReducer
+ * @desc a reducer function
+ * Accepts payload from action creator
+ * @param {Object} state
+ * @param {Object} action
+ * @returns state, payload, and error message
+ * @category Frontend
+ */
+
 export const productCreateReducer = (state = {}, action) => {
   switch (action.type) {
     case PRODUCT_CREATE_REQUEST:
@@ -78,6 +122,36 @@ export const productCreateReducer = (state = {}, action) => {
 
     case PRODUCT_CREATE_RESET:
       return {};
+
+    default:
+      return state;
+  }
+};
+
+/**
+ * @function productUpdateReducer
+ * @desc a reducer function
+ * Accepts payload from action creator
+ * @param {Object} state
+ * @param {Object} action
+ * @returns state, payload, and error message
+ * @category Frontend
+ */
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCT_UPDATE_REQUEST:
+      return { loading: true };
+
+    case PRODUCT_UPDATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload };
+
+    case PRODUCT_UPDATE_FAIL:
+      return { loading: false, error: action.payload };
+
+    case PRODUCT_UPDATE_RESET:
+      return { product: {} };
+
     default:
       return state;
   }
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { listProductDetails } from '../actions/productActions';
+import { listProductDetails, updateProduct } from '../actions/productActions';
 import { Button, Form } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
 import { Link } from 'react-router-dom';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants';
 
 const ProductEditScreen = ({ match, history }) => {
   const productId = match.params.id;
@@ -24,22 +25,46 @@ const ProductEditScreen = ({ match, history }) => {
   const productDetails = useSelector(state => state.productDetails);
   const { loading, error, product } = productDetails;
 
+  const productUpdate = useSelector(state => state.productUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdate;
+
   useEffect(() => {
-    if (!product.name && product._id !== productId) {
-      dispatch(listProductDetails(productId));
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET });
+      history.push('/admin/productlist');
     } else {
-      setName(product.name);
-      setPrice(product.price);
-      setImage(product.image);
-      setBrand(product.brand);
-      setCategory(product.category);
-      setDescription(product.category);
-      setCountInStock(product.countInStock);
+      if (!product.name && product._id !== productId) {
+        dispatch(listProductDetails(productId));
+      } else {
+        setName(product.name);
+        setPrice(product.price);
+        setImage(product.image);
+        setBrand(product.brand);
+        setCategory(product.category);
+        setDescription(product.description);
+        setCountInStock(product.countInStock);
+      }
     }
-  }, [dispatch, product, productId]);
+  }, [dispatch, history, product, productId, successUpdate]);
 
   const submitHandler = e => {
     e.preventDefault();
+    dispatch(
+      updateProduct({
+        _id: productId,
+        name,
+        price,
+        image,
+        brand,
+        category,
+        description,
+        countInStock,
+      })
+    );
   };
   return (
     <>
@@ -48,6 +73,8 @@ const ProductEditScreen = ({ match, history }) => {
       </Link>
       <FormContainer>
         <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
         {loading ? (
           <Loader />
         ) : error ? (
