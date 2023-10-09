b67432cb7e2c8dd3629e35e3ae3b37e6bf6eb210
Mozahedul <mozahed001@gmail.com>
2023-10-09 17:30:23 +0600

    remove / from cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ['https://ecommerce-backend-nine-xi.vercel.app'],
+    methods: ['POST', 'GET'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
