012dffae790bd8fb8160ce544776ba8f8e92a67d
Mozahedul <mozahed001@gmail.com>
2023-10-09 16:49:24 +0600

    Product map problem resolved

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -22,18 +22,20 @@ const ProductCarousel = () => {
     <Message variant="danger">{error}</Message>
   ) : (
     <Carousel pause="hover" className="bg-dark">
-      {products.map(product => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} />
-            <Carousel.Caption className="carousel-caption">
-              <h2>
-                {product.name} (${product.price})
-              </h2>
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
+      {Array.isArray(products) &&
+        products.length > 0 &&
+        products.map(product => (
+          <Carousel.Item key={product._id}>
+            <Link to={`/product/${product._id}`}>
+              <Image src={product.image} alt={product.name} />
+              <Carousel.Caption className="carousel-caption">
+                <h2>
+                  {product.name} (${product.price})
+                </h2>
+              </Carousel.Caption>
+            </Link>
+          </Carousel.Item>
+        ))}
     </Carousel>
   );
 };
